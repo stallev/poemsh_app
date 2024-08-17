fb7811f78d9a3675c6464f11bde52803a5b3e100
Alexander <stwork87@gmail.com>
2024-08-17 20:51:52 +0300

    deleted unused hooks4

--- src/modules/Header/Header.tsx
+++ src/modules/Header/Header.tsx
@@ -1,4 +1,3 @@
-'use client'
 import { MainNav } from '@/components/MainNav/MainNav';
 import { Container } from '@/containers/Container/Container';
 import { Logo } from '@/components/CustomSharedUI/Logo/Logo';
