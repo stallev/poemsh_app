c865aa4ac34368d71f211e2b5a4505e5fd72d0aa
Alexander <stwork87@gmail.com>
2024-08-31 23:35:38 +0300

    P4: Add update post page

--- src/app/[lang]/posts/update-post/[id]/page.tsx
+++ src/app/[lang]/posts/update-post/[id]/page.tsx
@@ -1,4 +1,6 @@
+import PostApiService from "@/services/api/PostsAPI";
 import { Locale } from "@/i18n.config";
+import { PostForm } from "@/components/PostForm/PostForm";
 
 interface Params {
   params: {
@@ -8,8 +10,14 @@ interface Params {
 }
 
 const UpdatePost = ({ params: { id, lang } }: Params) => {
+  const postData = PostApiService.getSinglePost(id);
+  if(!postData) return;
   return(
-    <h1>{`Post id is ${id}, current language - ${lang}`}</h1>
+    <div className='flex flex-col gap-6'>
+      <h1>{`Post id is ${id}, current language - ${lang}`}</h1>
+      <p>{postData?.description && postData?.description}</p>
+      <PostForm data={postData} />
+    </div>
   )
 }
 
